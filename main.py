743ae97edbbf0e4ca78221fc4eb69b62ea91e7de
MartynaSobolewska <martyna.sobol.22@gmail.com>
2020-10-27 00:25:16 +0000

    logging into microsoft account, going through all subpages until panopto's video

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 import docx
+import time
 
 
 # data from user
+
 file_path = ""
 document_name = ""
 generate_definitions_table = False
 panopto_link = ""
+login = ""
+password = ""
 
 
-def get_data_from_user():
-    global file_path, document_name, generate_definitions_table, panopto_link
+def get_basic_data_from_user():
+    global file_path, document_name, generate_definitions_table, panopto_link, login, password
     validate = URLValidator()
     doc = docx.Document()
 
@@ -21,8 +26,10 @@ def get_data_from_user():
         panopto_link = input("Please paste link to the Panopto video: ")
         try:
             validate(panopto_link)
-            if panopto_link.__contains__("panopto.eu")
+            if panopto_link.__contains__("panopto.eu"):
                 link_is_valid = True
+            else:
+                print("not a Panopto link.")
         except ValidationError:
             print("Incorrect link.")
 
@@ -30,7 +37,6 @@ def get_data_from_user():
     while not document_can_be_created:
         document_name = input("Please enter the name of the word file you wish to create: ")
         file_path = input("...and directory path (where you want to create it): ")
-        print(file_path + "\\" + document_name)
         try:
             doc.add_heading(document_name, 0)
             doc.save(file_path + "\\" + document_name + ".docx")
@@ -38,11 +44,37 @@ def get_data_from_user():
         except:
             print("incorrect path or file name.")
 
+    login = input("Please enter your uni's email: ")
+    password = input("Please enter your uni's account password: ")
 
-get_data_from_user()
+
+def microsoft_login(driver):
+    global login, password
+    try:
+        email_textbox = driver.find_element_by_name("loginfmt")
+        email_textbox.send_keys(login)
+        email_textbox.send_keys(Keys.RETURN)
+        # goes to password page
+        password_textbox = driver.find_element_by_name("passwd")
+        password_textbox.send_keys(password)
+        time.sleep(2)
+        password_button = driver.find_element_by_id("idSIButton9")
+        password_button.send_keys(Keys.RETURN)
+        # do you want to remember next time: no
+        not_remember_button = driver.find_element_by_id("idBtn_Back")
+        not_remember_button.send_keys(Keys.RETURN)
+    except:
+        print("wrong login or password.")
+
+
+get_basic_data_from_user()
 
 # selenium setup
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)  # PATH is location of chrome web driver needed to use selenium
 driver.get(panopto_link)
-
+login_button = driver.find_element_by_id("PageContentPlaceholder_loginControl_externalLoginButton")
+login_button.send_keys(Keys.RETURN) # press enter
+# now, microsoft login page pops out
+microsoft_login(driver)
+#panopto video page
